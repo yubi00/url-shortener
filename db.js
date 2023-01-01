2300518b3d14b0c4c962272864c1fd0064afbdc9
yubi00 <ubrajkhadka@gmail.com>
2023-01-01 23:24:24 +1100

    add env var for DATABASE_URL

--- db.js
+++ db.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose')
-const Url = require('./models/url')
-const Counter = require('./models/counter')
+const mongoose = require('mongoose');
+const Url = require('./models/url');
+const Counter = require('./models/counter');
 
 mongoose
-  .connect('mongodb://mongo:27017/docker-node-mongo', {
+  .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
-    console.log('connected to database')
+    console.log('connected to database');
 
     //delete all  previous url entries to start fresh
     Url.deleteMany({}, () => {
-      console.log('url collection removed')
-    })
+      console.log('url collection removed');
+    });
 
     //initialize counter collection with initial count value of 10000
     Counter.deleteMany({}, () => {
-      console.log('counter collection removed')
-      const counter = new Counter({ _id: 'url_count', count: 10000 })
+      console.log('counter collection removed');
+      const counter = new Counter({ _id: 'url_count', count: 10000 });
       counter.save((err) => {
-        if (err) return console.log(err)
-        console.log('counter saved')
-      })
-    })
+        if (err) return console.log(err);
+        console.log('counter saved');
+      });
+    });
   })
   .catch((err) => {
-    console.log('error connecting to database')
-  })
+    console.log('error connecting to database');
+  });
